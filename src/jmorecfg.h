36c96f5a824e732640a955e97e497b39f55d653b
Dirk Lemstra <dirk@lemstra.org>
2025-07-15 21:12:29 +0200

    Fixed exports.

--- src/jmorecfg.h
+++ src/jmorecfg.h
@@ -171,10 +171,14 @@ typedef unsigned int JDIMENSION;
 /* a function referenced thru EXTERNs: */
 #define GLOBAL(type)            type
 /* a reference to a GLOBAL function: */
-#if defined(_DLL) && !defined(_LIB)
+#if defined(_LIB)
+#define EXTERN(type)            extern type
+#else
+#if defined(JPEG_TURBO_DLL_BUILD)
 #define EXTERN(type)            extern __declspec(dllexport) type
 #else
-#define EXTERN(type)            extern type
+#define EXTERN(type)            extern __declspec(dllimport) type
+#endif
 #endif
 
 
